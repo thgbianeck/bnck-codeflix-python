7f734108fe6f6d681b5693b449a8a3219e86154b
Thaigo Bianeck <thgbianeck@gmail.com>
2024-08-05 01:09:03 -0300

    AJustes de Repositório

---
+++ category.py
@@ -0,0 +1,48 @@
+from dataclasses import dataclass, field
+import uuid
+from uuid import UUID
+
+@dataclass
+class Category:
+    name: str
+    description: str = ""
+    is_active: bool = True
+    id: UUID = field(default_factory=uuid.uuid4)
+
+    def __post_init__(self):
+        self.validate()
+
+    def validate(self):
+        if len(self.name) > 255:
+            raise ValueError("name cannot be longer than 255")
+
+        if not self.name:  # len(self.name) == 0
+            raise ValueError("name cannot be empty")
+
+    def __str__(self):
+        return f"{self.name} - {self.description} ({self.is_active})"
+
+    def __repr__(self):
+        return f"<Category {self.name} ({self.id})>"
+
+    def __eq__(self, other):  # a == b -> a.__eq__(b)
+        if not isinstance(other, Category):
+            return False
+
+        return self.id == other.id
+
+    def update_category(self, name, description):
+        self.name = name
+        self.description = description
+
+        self.validate()
+
+    def activate(self):
+        self.is_active = True
+
+        self.validate()
+
+    def deactivate(self):
+        self.is_active = False
+
+        self.validate()
---
+++ test_category.py
@@ -0,0 +1,134 @@
+import pytest
+from uuid import UUID
+import uuid
+
+from category import Category
+
+
+class TestCategory:
+    def test_name_is_required(self):
+        with pytest.raises(TypeError, match="missing 1 required positional argument: 'name'"):
+            Category()
+
+    def test_name_must_have_less_than_255_characters(self):
+        with pytest.raises(ValueError, match="name cannot be longer than 255"):
+            Category(name="a" * 256)
+
+    def test_category_must_be_created_with_id_as_uuid_by_default(self):
+        category = Category(name="Filme")
+        assert isinstance(category.id, UUID)
+
+    def test_create_category_with_default_values(self):
+        category = Category(name="Filme")
+        assert category.name == "Filme"
+        assert category.description == ""
+        assert category.is_active is True
+
+    def test_create_category_as_active_by_default(self):
+        category = Category(name="Filme")
+        assert category.is_active is True
+
+    def test_create_category_with_provided_values(self):
+        cat_id = uuid.uuid4()
+        category = Category(
+            id=cat_id,
+            name="Filme",
+            description="Filmes em geral",
+            is_active=False,
+        )
+
+        assert category.id == cat_id
+        assert category.name == "Filme"
+        assert category.description == "Filmes em geral"
+        assert category.is_active is False
+
+    def test_cannot_create_category_with_empty_name(self):
+        with pytest.raises(ValueError, match="name cannot be empty"):
+            Category(name="")
+
+class TestUpdateCategory:
+    def test_update_category_with_name_and_description(self):
+        category = Category(name="Filme", description="Filmes em geral")
+
+        category.update_category(name="Série", description="Séries em geral")
+
+        assert category.name == "Série"
+        assert category.description == "Séries em geral"
+
+    def test_update_category_with_invalid_name_raises_exception(self):
+        category = Category(name="Filme", description="Filmes em geral")
+
+        with pytest.raises(ValueError, match="name cannot be longer than 255"):
+            category.update_category(name="a" * 256, description="Séries em geral")
+
+    def test_cannot_update_category_with_empty_name(self):
+        with pytest.raises(ValueError, match="name cannot be empty"):
+            Category(name="")
+
+
+class TestActivate:
+    def test_activate_inactive_category(self):
+        category = Category(
+            name="Filme",
+            description="Filmes em geral",
+            is_active=False,
+        )
+
+        category.activate()
+
+        assert category.is_active is True
+
+    def test_activate_active_category(self):
+        category = Category(
+            name="Filme",
+            description="Filmes em geral",
+            is_active=True,
+        )
+
+        category.activate()
+
+        assert category.is_active is True
+
+
+class TestDeactivate:
+    def test_deactivate_active_category(self):
+        category = Category(
+            name="Filme",
+            description="Filmes em geral",
+            is_active=True,
+        )
+
+        category.deactivate()
+
+        assert category.is_active is False
+
+    def test_deactivate_inactive_category(self):
+        category = Category(
+            name="Filme",
+            description="Filmes em geral",
+            is_active=False,
+        )
+
+        category.deactivate()
+
+        assert category.is_active is False
+
+
+class TestEquality:
+    def test_when_categories_have_same_id_they_are_equal(self):
+        common_id = uuid.uuid4()
+        category_1 = Category(name="Filme", id=common_id)
+        category_2 = Category(name="Filme", id=common_id)
+
+        assert category_1 == category_2
+
+    def test_equality_different_classes(self):
+        class Dummy:
+            pass
+
+        common_id = uuid.uuid4()
+        category = Category(name="Filme", id=common_id)
+        dummy = Dummy()
+        dummy.id = common_id
+
+        assert category != dummy
